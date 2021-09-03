124c3a861942643a7d822f7a0ece4f125fc49795
Weihang Lo <weihang.lo@suse.com>
2021-09-03 16:39:24 +0800

    Break chroot jail to mount filesystem and access devices info
    As a container-attached storage solution, if NDM want to deal with
    filesystem and device related tasks, it need to break the chroot jail.
    
    In this commit, we
    
    - Reuse go-iscsi-helper to run command in host namespace on demand.
      "On demend" means NDM detects whether one mounts host's `/proc` onto
      `/host/proc` inside the container, it will get the ability to retrieve
      proccess info on the host and enter host namespace to mount filesystem.
    - Add `parted`, `util-linux`, and `e2fsprogs` pkg for docker image.
      (We need commands `parted`, `lsblk`, and `mkfs.ext4` at this moment.)
    
    Signed-off-by: Weihang Lo <weihang.lo@suse.com>

--- pkg/disk/mount_linux.go
+++ pkg/disk/mount_linux.go
@@ -4,6 +4,9 @@ import (
 	"os"
 	"strings"
 	"syscall"
+
+	iscsiutil "github.com/longhorn/go-iscsi-helper/util"
+	"github.com/longhorn/node-disk-manager/pkg/util"
 )
 
 var ext4MountOptions = strings.Join([]string{
@@ -15,16 +18,34 @@ var ext4MountOptions = strings.Join([]string{
 
 // MountDisk mounts the specified ext4 volume device to the specified path
 func MountDisk(devPath, mountPoint string) error {
+	isHostProcMounted, err := util.IsHostProcMounted()
+	if err != nil {
+		return err
+	}
+	if isHostProcMounted {
+		if _, err := executeOnHostNamespace("mkdir", []string{"-p", mountPoint}); err != nil {
+			return err
+		}
+		return mountExt4OnHostNamespace(devPath, mountPoint, false)
+	}
+
 	if err := os.MkdirAll(mountPoint, os.ModeDir); err != nil {
 		return err
 	}
-
 	return mountExt4(devPath, mountPoint, false)
 }
 
 // UmountDisk unmounts the specified volume device to the specified path
 func UmountDisk(path string) error {
-	err := syscall.Unmount(path, 0)
+	isHostProcMounted, err := util.IsHostProcMounted()
+	if err != nil {
+		return err
+	}
+	if isHostProcMounted {
+		_, err := executeOnHostNamespace("umount", []string{path})
+		return err
+	}
+	err = syscall.Unmount(path, 0)
 	return os.NewSyscallError("umount", err)
 }
 
@@ -36,4 +57,30 @@ func mountExt4(device, path string, readonly bool) error {
 	}
 	err := syscall.Mount(device, path, "ext4", flags, ext4MountOptions)
 	return os.NewSyscallError("mount", err)
+}
+
+// mountExt4OnHostNamespace provides the same functionality as mountExt4 but on host namespace.
+func mountExt4OnHostNamespace(device, path string, readonly bool) error {
+	ns := iscsiutil.GetHostNamespacePath(util.HostProcPath)
+	executor, err := iscsiutil.NewNamespaceExecutor(ns)
+	if err != nil {
+		return err
+	}
+
+	opts := ext4MountOptions + ",relatime"
+	if readonly {
+		opts = opts + ",ro"
+	}
+
+	_, err = executor.Execute("mount", []string{"-t", "ext4", "-o", opts, device, path})
+	return err
+}
+
+func executeOnHostNamespace(cmd string, args []string) (string, error) {
+	ns := iscsiutil.GetHostNamespacePath(util.HostProcPath)
+	executor, err := iscsiutil.NewNamespaceExecutor(ns)
+	if err != nil {
+		return "", err
+	}
+	return executor.Execute(cmd, args)
 }
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -2,8 +2,25 @@ package util
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
+
+const (
+	// ProcPath is a vfs storing process info for Linux.
+	ProcPath = "/proc"
+	// HostProcPath is the convention path where host `/proc` is mounted.
+	HostProcPath = "/host/proc"
+)
+
+// IsHostProcMounted checks if host's proc info `/proc` is mounted on `/host/proc`
+func IsHostProcMounted() (bool, error) {
+	_, err := os.Stat(HostProcPath)
+	if err != nil && !os.IsNotExist(err) {
+		return false, err
+	}
+	return err == nil, nil
+}
 
 func GetFullDevPath(shortPath string) string {
 	if shortPath == "" {
