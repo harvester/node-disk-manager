ef7474815be7ccb335db612d035f4b9b0df0ab48
Weihang Lo <weihang.lo@suse.com>
2021-09-27 12:31:22 +0800

    Get filesystem label from lsblk for partition
    Signed-off-by: Weihang Lo <weihang.lo@suse.com>

--- pkg/block/util.go
+++ pkg/block/util.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 func GetParentDevName(devPath string) (string, error) {
@@ -23,6 +25,29 @@ func GetParentDevName(devPath string) (string, error) {
 
 	return strings.TrimSuffix(string(out), "\n"), nil
 }
+
 func HasPartitions(disk *Disk) bool {
 	return len(disk.Partitions) > 0
+}
+
+func GetFileSystemLabel(devPath string) string {
+	if !strings.HasPrefix(devPath, "/dev") {
+		devPath = "/dev/" + devPath
+	}
+	args := []string{
+		"lsblk",
+		"-no",
+		"label",
+		devPath,
+	}
+	out, err := exec.Command(args[0], args[1:]...).Output()
+	if err != nil {
+		logrus.Warnf("failed to get filesystem label for device %s, %s", devPath, err.Error())
+		return ""
+	}
+	splited := strings.SplitN(string(out), "\n", 2)
+	if len(splited) > 0 {
+		return splited[0]
+	}
+	return ""
 }
