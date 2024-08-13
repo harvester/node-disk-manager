b1e3382c79359e3e2d07808d0db5361f7b3f5fd2
Vicente Cheng <vicente.cheng@suse.com>
2024-08-13 18:00:39 +0800

    common: move to wrangler v3 and update the generated codes
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- pkg/generated/controllers/harvesterhci.io/v1beta1/interface.go
+++ pkg/generated/controllers/harvesterhci.io/v1beta1/interface.go
@@ -21,8 +21,8 @@ package v1beta1
 import (
 	v1beta1 "github.com/harvester/node-disk-manager/pkg/apis/harvesterhci.io/v1beta1"
 	"github.com/rancher/lasso/pkg/controller"
-	"github.com/rancher/wrangler/v2/pkg/generic"
-	"github.com/rancher/wrangler/v2/pkg/schemes"
+	"github.com/rancher/wrangler/v3/pkg/generic"
+	"github.com/rancher/wrangler/v3/pkg/schemes"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
