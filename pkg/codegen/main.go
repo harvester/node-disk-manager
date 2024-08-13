c7366a4ede4cea52ede4f948c232b0042904081b
Vicente Cheng <vicente.cheng@suse.com>
2024-08-13 18:00:39 +0800

    common: move to wrangler v3 and update the generated codes
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>
    (cherry picked from commit b3d80ccebad816e636091a89781227a784be50c1)

--- pkg/codegen/main.go
+++ pkg/codegen/main.go
@@ -4,8 +4,8 @@ import (
 	"os"
 
 	longhornv1 "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
-	controllergen "github.com/rancher/wrangler/v2/pkg/controller-gen"
-	"github.com/rancher/wrangler/v2/pkg/controller-gen/args"
+	controllergen "github.com/rancher/wrangler/v3/pkg/controller-gen"
+	"github.com/rancher/wrangler/v3/pkg/controller-gen/args"
 
 	diskv1 "github.com/harvester/node-disk-manager/pkg/apis/harvesterhci.io/v1beta1"
 )
