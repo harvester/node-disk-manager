57ce9dbdf0fdd16b2590be1f729bdf82ca78d297
Vicente Cheng <vicente.cheng@suse.com>
2024-09-27 09:12:01 +0800

    scanner: correct the autoProvision mechanism
        - we should not set both `Spec.Filesystem.Provisioned` and
          `Spec.Provision` or the remove operation would be blocked if
          we only change one of these to false.
    
        This is a side-effect that comes from 1877a4ef1743e4bb01b81179aacfe5b32da059ed.
        For now, we will remove one of these two fields to ensure the delete operation
        work. We still need mutator to help convert these two fields.
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>
    (cherry picked from commit d5b7347189fcf0b7d65bf8d844dc570c1c3bbb7b)

--- pkg/controller/blockdevice/scanner.go
+++ pkg/controller/blockdevice/scanner.go
@@ -292,7 +292,6 @@ func (s *Scanner) SaveBlockDevice(bd *diskv1.BlockDevice, autoProvisioned bool) 
 			if autoProvisioned {
 				bd.Spec.FileSystem.ForceFormatted = true
 				bd.Spec.FileSystem.Provisioned = true
-				bd.Spec.Provision = true
 			}
 			logrus.Infof("Add new block device %s with device: %s", bd.Name, bd.Spec.DevPath)
 			return s.Blockdevices.Create(bd)
