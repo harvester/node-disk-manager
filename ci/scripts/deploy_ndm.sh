e35cbb3934daafe923eba6f99e21b37ccb9f92d4
Vicente Cheng <vicente.cheng@suse.com>
2024-09-30 17:54:14 +0800

    ci: add integration for fresh installation
        - Also, we make two jobs can run parallel
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- ci/scripts/deploy_ndm.sh
+++ ci/scripts/deploy_ndm.sh
@@ -73,9 +73,13 @@ ensure_longhorn_ready
 
 pushd $TOP_DIR
 
-cat >> ndm-override.yaml << 'EOF'
+cat >> ndm-override.yaml.default << 'EOF'
 autoProvisionFilter: [/dev/sd*]
 EOF
+
+if [ ! -f ndm-override.yaml ]; then
+  mv ndm-override.yaml.default ndm-override.yaml
+fi
 
 $HELM pull harvester-node-disk-manager --repo https://charts.harvesterhci.io --untar
 $HELM install -f $TOP_DIR/ndm-override.yaml harvester-node-disk-manager ./harvester-node-disk-manager --create-namespace -n harvester-system
