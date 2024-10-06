cbd3cfa4899eb5df84ff57540e6999e91cc7e428
Vicente Cheng <vicente.cheng@suse.com>
2024-10-07 00:42:39 +0800

    ci: improve the ci for webhook installation
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- ci/scripts/deploy_ndm_current.sh
+++ ci/scripts/deploy_ndm_current.sh
@@ -18,8 +18,32 @@ wait_ndm_ready() {
       echo "harvester-node-disk-manager pods are ready!"
       break
     fi
-    echo "harvester-node-disk-manager pods are not ready (${running_num}/${cluster_nodes}), sleep 10 seconds."
+    echo "harvester-node-disk-manager pods are not ready, sleep 10 seconds."
     sleep 10
+  done
+}
+
+ensure_longhorn_ready() {
+  # ensure longhorn-manager first
+  while [ true ]; do
+    running_num=$(kubectl get ds longhorn-manager -n longhorn-system -o 'jsonpath={.status.numberReady}')
+    if [[ $running_num -eq ${cluster_nodes} ]]; then
+      echo "longhorn-manager pods are ready!"
+      break
+    fi
+    echo "check longhorn-manager failure, please deploy longhorn first."
+    exit 1
+  done
+
+  # ensure instance-manager-e ready
+  while [ true ]; do
+    running_num=$(kubectl get pods -n longhorn-system |grep ^instance-manager |grep Running |awk '{print $3}' |wc -l)
+    if [[ $running_num -eq ${cluster_nodes} ]]; then
+      echo "instance-manager pods are ready!"
+      break
+    fi
+    echo "check instance-manager failure, please deploy longhorn first."
+    exit 1
   done
 }
 
@@ -45,31 +69,23 @@ fi
 
 cluster_nodes=$(yq -e e '.cluster_size' $TOP_DIR/settings.yaml)
 echo "cluster nodes: $cluster_nodes"
+ensure_longhorn_ready
 
 pushd $TOP_DIR
-# cleanup first
-rm -rf harvester-node-disk-manager*
-rm -rf ndm-override.yaml
+cat >> ndm-override.yaml.default << 'EOF'
+autoProvisionFilter: [/dev/sd*]
+EOF
+
+if [ ! -f ndm-override.yaml ]; then
+  mv ndm-override.yaml.default ndm-override.yaml
+fi
 
 cp -r ../deploy/charts/harvester-node-disk-manager harvester-node-disk-manager
-cp ../ci/charts/ndm-override.yaml ndm-override.yaml
 
 target_img=$(yq -e .image.repository ndm-override.yaml)
-echo "upgrade target image: ${target_img}, upgrading ..."
-$HELM upgrade -f $TOP_DIR/ndm-override.yaml harvester-node-disk-manager harvester-node-disk-manager/ -n harvester-system
-
-sleep 30 # wait 30 seconds for ndm respawn pods
+echo "install target image: ${target_img}"
+$HELM install -f $TOP_DIR/ndm-override.yaml harvester-node-disk-manager ./harvester-node-disk-manager --create-namespace -n harvester-system
 
 wait_ndm_ready
-# check image
-pod_name=$(kubectl get pods -n harvester-system |grep Running |grep -v webhook |grep ^harvester-node-disk-manager|head -n1 |awk '{print $1}')
-container_img=$(kubectl get pods ${pod_name} -n harvester-system -o yaml |yq -e .spec.containers[0].image |tr ":" \n)
-yaml_img=$(yq -e .image.repository ndm-override.yaml)
-if grep -q ${yaml_img} <<< ${container_img}; then
-  echo "Image is equal: ${yaml_img}"
-else
-  echo "Image is non-equal, container: ${container_img}, yaml file: ${yaml_img}"
-  exit 1
-fi
-echo "harvester-node-disk-manager upgrade successfully!"
+echo "harvester-node-disk-manager is ready"
 popd
