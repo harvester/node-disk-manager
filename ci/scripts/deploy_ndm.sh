698ad649b56b9b5d1b080e58a02fb43c3ecbd2c8
Vicente Cheng <vicente.cheng@suse.com>
2024-12-03 11:54:22 +0800

    ci: specify the chart version 0.6.0
        - new version introduce some new fields and components that will
          cause the CI failure
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- ci/scripts/deploy_ndm.sh
+++ ci/scripts/deploy_ndm.sh
@@ -81,7 +81,7 @@ if [ ! -f ndm-override.yaml ]; then
   mv ndm-override.yaml.default ndm-override.yaml
 fi
 
-$HELM pull harvester-node-disk-manager --repo https://charts.harvesterhci.io --untar
+$HELM pull harvester-node-disk-manager --repo https://charts.harvesterhci.io --untar --version 0.6.0
 $HELM install -f $TOP_DIR/ndm-override.yaml harvester-node-disk-manager ./harvester-node-disk-manager --create-namespace -n harvester-system
 
 wait_ndm_ready
