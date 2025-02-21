b95996f726adced5573a16b00e12e3ba335f25bc
Vicente Cheng <vicente.cheng@suse.com>
2025-02-21 14:47:36 +0800

    test/integration: improve the integration test
        - Now, we will skip other tests once the current test fail.
          That we could focus on the logs to figure out why.
        - Also, create the helper.sh to reuse the duplicated functions
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>
    (cherry picked from commit b1b011de951c86116536b15e8ce0284ca269fd81)

--- ci/scripts/deploy_ndm_chart.sh
+++ ci/scripts/deploy_ndm_chart.sh
@@ -1,74 +1,7 @@
 #!/bin/bash -e
 
-TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" &> /dev/null && pwd )"
+source helpers.sh
 
-ensure_command() {
-  local cmd=$1
-  if ! which $cmd &> /dev/null; then
-    echo 1
-    return
-  fi
-  echo 0
-}
-
-wait_ndm_ready() {
-  while [ true ]; do
-    running_num=$(kubectl get ds harvester-node-disk-manager -n harvester-system -o 'jsonpath={.status.numberReady}')
-    if [[ $running_num -eq ${cluster_nodes} ]]; then
-      echo "harvester-node-disk-manager pods are ready!"
-      break
-    fi
-    echo "harvester-node-disk-manager pods are not ready, sleep 10 seconds."
-    sleep 10
-  done
-}
-
-ensure_longhorn_ready() {
-  # ensure longhorn-manager first
-  while [ true ]; do
-    running_num=$(kubectl get ds longhorn-manager -n longhorn-system -o 'jsonpath={.status.numberReady}')
-    if [[ $running_num -eq ${cluster_nodes} ]]; then
-      echo "longhorn-manager pods are ready!"
-      break
-    fi
-    echo "check longhorn-manager failure, please deploy longhorn first."
-    exit 1
-  done
-
-  # ensure instance-manager-e ready
-  while [ true ]; do
-    running_num=$(kubectl get pods -n longhorn-system |grep ^instance-manager |grep Running |awk '{print $3}' |wc -l)
-    if [[ $running_num -eq ${cluster_nodes} ]]; then
-      echo "instance-manager pods are ready!"
-      break
-    fi
-    echo "check instance-manager failure, please deploy longhorn first."
-    exit 1
-  done
-}
-
-if [ ! -f $TOP_DIR/kubeconfig ]; then
-  echo "kubeconfig does not exist. Please create cluster first."
-  echo "Maybe try new_cluster.sh"
-  exit 1
-fi
-echo $TOP_DIR/kubeconfig
-export KUBECONFIG=$TOP_DIR/kubeconfig
-
-if [[ $(ensure_command helm) -eq 1 ]]; then
-  echo "no helm, try to curl..."
-  curl -O https://get.helm.sh/helm-v3.9.4-linux-amd64.tar.gz
-  tar -zxvf helm-v3.9.4-linux-amd64.tar.gz
-  HELM=$TOP_DIR/linux-amd64/helm
-  $HELM version
-else
-  echo "Get helm, version info as below"
-  HELM=$(which helm)
-  $HELM version
-fi
-
-cluster_nodes=$(yq -e e '.cluster_size' $TOP_DIR/settings.yaml)
-echo "cluster nodes: $cluster_nodes"
 ensure_longhorn_ready
 
 pushd $TOP_DIR
