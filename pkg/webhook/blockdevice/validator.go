bfdaaca2639460bfe3a3c842dfdaf21d7da784fc
Martin Dekov <martin.dekov@suse.com>
2025-09-19 15:43:51 +0300

    Validate disk removals
    Rejecting disk removals when there are attached volumes in Degraded
    state on single node harvester set ups.
    
    We also make sure to reject removals when virtualmachineimages are
    in failed state as well.
    
    The logic is split in two parts:
    * We take the disk selectors of volumes in degraded state and
    compare the selectors against the disktags
    * We take the disk selectors of virtualmachineimages in failed
    state and compare against disk tags of disk
    
    In both scenarios in case there is match while disk is being
    disabled, we list to the user which volumes and then images
    needs to get fixed.
    
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>
    
    Address feedback from Moritz
    
    Addressing feedback form Moritz with suggestion to use
    get for specific volumes which are degraded.
    
    Initial assumption was that get would reach out kubernetes
    API for cache, but he clairified that cache is local so calls
    don't weight on the API.
    
    Switched to that.
    
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>
    
    Skip checks in case of missing tags
    
    Skipping checks in case of missing tags as if storage is missing tags
    then its either the default one which comes with installation or is
    not being used as the tag is the selector which matches the objects
    to the disk
    
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>
    
    Fix nil pointer and rbac with node access
    
    Fixing nil pointer by checking the Provisioner of both
    old and new objects as those can be nil and then panic
    would be handled silently.
    
    Also since we have cache for nodes added added nodes
    to the cluster role
    
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>
    
    Add feedback from Jian
    
    Adding feedback from Jian and fixing potential nil pointer
    on pv spec CSI, which can be nil before checking it's
    selector
    
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>
    
    Address feedback for multiple objects per tag
    
    Addressing additional feedback where I returned the last
    problematic object for the tag as opposed to a list of
    objects related with the tag.
    
    Also small optimization to skip logic in case vm image
    is not in a failed state.
    
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>

--- pkg/webhook/blockdevice/validator.go
+++ pkg/webhook/blockdevice/validator.go
@@ -3,8 +3,10 @@ package blockdevice
 import (
 	"fmt"
 
+	lhv1beta2 "github.com/harvester/harvester/pkg/generated/controllers/longhorn.io/v1beta2"
 	werror "github.com/harvester/webhook/pkg/error"
 	"github.com/harvester/webhook/pkg/server/admission"
+	lhv1 "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
 	ctlcorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
 	ctlstoragev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/storage/v1"
 	"github.com/sirupsen/logrus"
@@ -13,6 +15,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 
+	ctlharvv1beta1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
 	diskv1 "github.com/harvester/node-disk-manager/pkg/apis/harvesterhci.io/v1beta1"
 	ctldiskv1 "github.com/harvester/node-disk-manager/pkg/generated/controllers/harvesterhci.io/v1beta1"
 	"github.com/harvester/node-disk-manager/pkg/utils"
@@ -24,13 +27,20 @@ type Validator struct {
 	BlockdeviceCache  ctldiskv1.BlockDeviceCache
 	storageClassCache ctlstoragev1.StorageClassCache
 	pvCache           ctlcorev1.PersistentVolumeCache
+	volumeCache       lhv1beta2.VolumeCache
+	nodeCache         ctlcorev1.NodeCache
+	vmImageCache      ctlharvv1beta1.VirtualMachineImageCache
 }
 
-func NewBlockdeviceValidator(blockdeviceCache ctldiskv1.BlockDeviceCache, storageClassCache ctlstoragev1.StorageClassCache, pvCache ctlcorev1.PersistentVolumeCache) *Validator {
+func NewBlockdeviceValidator(blockdeviceCache ctldiskv1.BlockDeviceCache, storageClassCache ctlstoragev1.StorageClassCache,
+	pvCache ctlcorev1.PersistentVolumeCache, volumeCache lhv1beta2.VolumeCache, nodeCache ctlcorev1.NodeCache, vmImageCache ctlharvv1beta1.VirtualMachineImageCache) *Validator {
 	return &Validator{
 		BlockdeviceCache:  blockdeviceCache,
 		storageClassCache: storageClassCache,
 		pvCache:           pvCache,
+		volumeCache:       volumeCache,
+		nodeCache:         nodeCache,
+		vmImageCache:      vmImageCache,
 	}
 }
 
@@ -45,10 +55,14 @@ func (v *Validator) Create(_ *admission.Request, newObj runtime.Object) error {
 func (v *Validator) Update(_ *admission.Request, oldObj, newObj runtime.Object) error {
 	newBd := newObj.(*diskv1.BlockDevice)
 	oldBd := oldObj.(*diskv1.BlockDevice)
+
 	if err := v.validateProvisioner(newBd); err != nil {
 		return err
 	}
-	return v.validateLVMProvisioner(oldBd, newBd)
+	if err := v.validateLVMProvisioner(oldBd, newBd); err != nil {
+		return err
+	}
+	return v.validateLHDisk(oldBd, newBd)
 }
 
 func (v *Validator) validateProvisioner(bd *diskv1.BlockDevice) error {
@@ -58,6 +72,28 @@ func (v *Validator) validateProvisioner(bd *diskv1.BlockDevice) error {
 
 	if bd.Spec.Provisioner.LVM != nil && bd.Spec.Provisioner.Longhorn != nil {
 		return werror.NewBadRequest("Blockdevice should not have multiple provisioners")
+	}
+	return nil
+}
+
+func (v *Validator) validateLHDisk(oldBd, newBd *diskv1.BlockDevice) error {
+	if oldBd.Spec.Provisioner != nil && newBd.Spec.Provisioner != nil &&
+		oldBd.Spec.Provisioner.Longhorn != nil && newBd.Spec.Provisioner.Longhorn != nil &&
+		oldBd.Spec.Provision && !newBd.Spec.Provision {
+		nodeList, err := v.nodeCache.List(labels.Everything())
+		if err != nil {
+			return err
+		}
+		if len(nodeList) == 1 && len(oldBd.Spec.Tags) > 0 {
+			err := v.validateDegradedVolumes(oldBd)
+			if err != nil {
+				return err
+			}
+			err = v.validateFailedVMImages(oldBd)
+			if err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
@@ -123,6 +159,81 @@ func (v *Validator) validateVGIsAlreadyUsed(bd *diskv1.BlockDevice) error {
 			errStr := fmt.Sprintf("There is already a PVC created using the target volume group (%v), so we cannot add or remove the associated blockdevices", targetVGName)
 			return werror.NewBadRequest(errStr)
 		}
+	}
+	return nil
+}
+
+func (v *Validator) validateDegradedVolumes(old *diskv1.BlockDevice) error {
+	volumeList, err := v.volumeCache.List(utils.LonghornSystemNamespaceName, labels.Everything())
+	if err != nil {
+		return err
+	}
+	if len(volumeList) == 0 {
+		return nil
+	}
+	degradedVolumes := make(map[string]struct{})
+	for _, vol := range volumeList {
+		if vol.Status.Robustness == lhv1.VolumeRobustnessDegraded {
+			degradedVolumes[vol.Name] = struct{}{}
+		}
+	}
+	if len(degradedVolumes) == 0 {
+		return nil
+	}
+	selectorDegradedVol := make(map[string][]string)
+	for name := range degradedVolumes {
+		pv, err := v.pvCache.Get(name)
+		if err != nil {
+			return err
+		}
+		diskSelector := ""
+		if pv.Spec.CSI != nil {
+			diskSelector = pv.Spec.CSI.VolumeAttributes[utils.DiskSelectorKey]
+		}
+		if len(diskSelector) != 0 {
+			selectorDegradedVol[diskSelector] = append(selectorDegradedVol[diskSelector], pv.Name)
+		}
+	}
+	degradedVolString := ""
+	for _, diskTag := range old.Spec.Tags {
+		if val, ok := selectorDegradedVol[diskTag]; ok {
+			degradedVolString += fmt.Sprintf(" %s: %v", diskTag, val)
+		}
+	}
+	if len(degradedVolString) > 0 {
+		return fmt.Errorf("the following tags with volumes:%s attached to disk: %s are in degraded state; evict disk before proceeding",
+			degradedVolString, old.Spec.DevPath)
+	}
+	return nil
+}
+
+func (v *Validator) validateFailedVMImages(old *diskv1.BlockDevice) error {
+	vmImageList, err := v.vmImageCache.List("", labels.Everything())
+	if err != nil {
+		return err
+	}
+	if len(vmImageList) == 0 {
+		return nil
+	}
+	selectorFailedVMIMage := make(map[string][]string)
+	for _, vmImage := range vmImageList {
+		if vmImage.Status.Failed == 0 {
+			continue
+		}
+		diskSelectorValue := vmImage.Spec.StorageClassParameters[utils.DiskSelectorKey]
+		if vmImage.Status.Failed != 0 && len(diskSelectorValue) > 0 {
+			selectorFailedVMIMage[diskSelectorValue] = append(selectorFailedVMIMage[diskSelectorValue], vmImage.Spec.DisplayName)
+		}
+	}
+	failedVMImages := ""
+	for _, diskTag := range old.Spec.Tags {
+		if val, ok := selectorFailedVMIMage[diskTag]; ok {
+			failedVMImages += fmt.Sprintf(" %s: %v", diskTag, val)
+		}
+	}
+	if len(failedVMImages) > 0 {
+		return fmt.Errorf("the following tags referenced by virtualmachineimages: %s attached to disk: %s are in failed state; evict disk before proceeding",
+			failedVMImages, old.Spec.DevPath)
 	}
 	return nil
 }
